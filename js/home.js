e9ef505bc9d643e941d4f3ad0a70accdd3fd85de
Chris Schiebelbein <c_schiebelbein@hotmail.com>
2022-04-08 21:27:50 -0600

    formatting

--- js/home.js
+++ js/home.js
@@ -1,45 +1,37 @@
-var auth = document.getElementById("home")
-var logout = document.getElementById("logout")
+var auth = document.getElementById("home");
+var logout = document.getElementById("logout");
 
-auth.innerText = 'test'
+auth.innerText = "test";
 
 function onAuth(response) {
+  let colArr = response.data.data;
 
-let colArr = response.data.data
-
-
-auth.innerHTML = ''
-colArr.forEach(el => {
-    console.log(el)
-    let div = document.createElement("div")
-    let p = document.createElement("p")
-    p.style.backgroundColor = `${el.color}`
-    p.append(`The color ${el.name} was created in the year ${el.year}`)
-    auth.append(p)
-});
+  auth.innerHTML = "";
+  colArr.forEach((el) => {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.style.backgroundColor = `${el.color}`;
+    p.append(`The color ${el.name} was created in the year ${el.year}`);
+    auth.append(p);
+  });
 }
 function onLogout() {
-    Cookies.remove('token')
-    authCheck()
+  Cookies.remove("token");
+  authCheck();
 }
 function onError(error) {
-    console.log(error)
+  console.log(error);
 }
 function authCheck() {
-    let token = Cookies.get('token')
+  let token = Cookies.get("token");
 
-    console.log(token)
-
-    if (token != 'QpwL5tke4Pnpja7X4') {
-        alert('invalid username (or password)')
-        window.location.assign("/index.html");
-    } else if (token == 'QpwL5tke4Pnpja7X4') {
-        auth.innerText = 'you did it you legend'
-        axios.get('https://reqres.in/api/unknown')
-        .then(onAuth).catch(onError)
-    }
+  if (token != "QpwL5tke4Pnpja7X4") {
+    alert("invalid username (or password)");
+    window.location.assign("/index.html");
+  } else if (token == "QpwL5tke4Pnpja7X4") {
+    axios.get("https://reqres.in/api/unknown").then(onAuth).catch(onError);
+  }
 }
 
-
-authCheck()
-logout.addEventListener('click', onLogout)
+authCheck();
+logout.addEventListener("click", onLogout);
